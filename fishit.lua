f87f45f8d982df836e8e0232d15028a2a4349f36
notrhy <cacingfz@gmail.com>
2025-10-11 07:55:01 +0700

    Update fishit.lua

--- fishit.lua
+++ fishit.lua
@@ -157,7 +157,7 @@ REReplicateTextEffect.OnClientEvent:Connect(function(payload)
     if busyCatch then return end
 
     busyCatch = true
-    task.wait(1.8)
+    task.wait(1.2)
     pcall(function() REFishingCompleted:FireServer() end)
 
     task.delay(COOLDOWN_CATCH, function() busyCatch = false end)
